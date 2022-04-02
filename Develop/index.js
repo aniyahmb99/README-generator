24c0f1c454ffabb55535c4ccbc8c03949eb1f03b
aniyahmb99 <aniyah_mb@yahoo.com>
2022-04-02 18:30:59 -0400

    initial commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./util/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your project's title?",
+    name: "Title",
+  },
+  {
+    type: "input",
+    message: " Please write a brief description of this project ",
+    name: "Description",
+  },
+  {
+    type: "input",
+    message: "Please provide a table of contents",
+    name: "Table of Contents",
+  },
+  {
+    type: "input",
+    message: "What is your Github username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your Github email address?",
+    name: "Github email",
+  },
+  {
+    type: "input",
+    message: "What is the test process for this project?",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: " Who are the contributors to this project?",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "Please provide instructions on how to install this project",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "How will this project be used? Please describe.",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Which license will you use for this project?",
+    name: "license",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then(generateMarkdown())
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log("Prompt couldn't be rendered in the current environment");
+      } else {
+        console.log("Something else went wrong");
+      }
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenseHas =
+
+function renderLicenseBadge(license) {
+  const license
+  if (questions[10].message === "") {
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +20,27 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `#Title 
+  # Description 
+  # Table of Contents 
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  # Installation
+  The following necessary dependencies must be installed to run the application properly: inquire, fs, util
+  # Usage
+  # License
+  
+  ![GitHub license]
+  # Contributing
+  # Tests
+  # Questions
+
 
 `;
 }
 
-module.exports = generateMarkdown;
+
